a9c0f242b82079d6017eef54d241f3fb83bf1982
Virgin Bitton <virgin.bitton@gmail.com>
2022-05-18 20:26:07 +0200

    update Condition model

--- contacts/models.py
+++ contacts/models.py
@@ -107,20 +107,34 @@ class Condition(models.Model):
     ]
 
     CHECK_STR_TYPES = [
-    ('IS', 'Contact field'),
-    ('IS NOT', 'Event triggered'),
-    ('CONTAINS', 'Page viewed'),
-    ('DOES NOT CONTAIN', 'Page viewed'),
-    ('EMPTY', 'Page viewed'),
-    ('NOT EMPTY', 'Page viewed'),
+    ('IS', 'is'),
+    ('IS NOT', 'is not'),
+    ('CONTAINS', 'contains'),
+    ('DOES NOT CONTAIN', 'does not contain'),
+    ('EMPTY', 'empty'),
+    ('NOT EMPTY', 'not empty'),
     ]
 
     CHECK_INT_TYPES = [
-    ('EQUALS', 'Contact field'),
-    ('SUPERIOR', 'Event triggered'),
-    ('SUPOREQUALS', 'Page viewed'),
-    ('INFERIOR', 'Page viewed'),
-    ('INFOREQUALS', 'Page viewed'),
+    ('EQUALS', 'equals'),
+    ('SUPERIOR', 'superior'),
+    ('SUPOREQUALS', 'superior or equals'),
+    ('INFERIOR', 'inferior'),
+    ('INFOREQUALS', 'inferior or equals'),
+    ]
+
+    CHECK_BOOL_TYPES = [
+    ('IS', 'is true'),
+    ('IS NOT', 'is false'),
+    ]
+
+    CHECK_DATE_TYPES = [
+    ('AT', 'At this date'),
+    ('BEFORE', 'Before this date'),
+    ('AFTER', 'After this date'),
+    ('EVER', 'Ever'),
+    ('LAST', 'In the last...'),
+    ('BETWEEN', 'Between ... and ...')
     ]
 
     group = models.ForeignKey(ConditionGroup, on_delete=models.CASCADE)
@@ -128,8 +142,11 @@ class Condition(models.Model):
     field_to_check = models.ForeignKey(CustomField, on_delete=models.CASCADE, null=True, blank=True)
     #event_to_check = models.ForeignKey(CustomField, on_delete=models.CASCADE, null=True, blank=True)
     #page_to_check = models.ForeignKey(CustomField, on_delete=models.CASCADE, null=True, blank=True)
+    check_condition_str = models.CharField(max_length=20, choices=CHECK_STR_TYPES, null=True, blank=True)
+    check_condition_int = models.CharField(max_length=20, choices=CHECK_INT_TYPES, null=True, blank=True)
+    check_condition_bool = models.CharField(max_length=20, choices=CHECK_BOOL_TYPES, null=True, blank=True)
+    check_condition_date = models.CharField(max_length=20, choices=CHECK_DATE_TYPES, null=True, blank=True)
     input_str = models.CharField(max_length=100, null=True, blank=True)
-    input_int = models.CharField(max_length=100, null=True, blank=True)
-
-    def __str__(self):
-        return self.name
+    input_at_least = models.IntegerField(null=True, blank=True)
+    input_date1 = models.DateField(null=True, blank=True)
+    input_date2 = models.DateField(null=True, blank=True)
